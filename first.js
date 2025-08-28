7b07eb216614928407e9fcdd4d27e372e563109e
jenish agravat <jenishagravat04@gmail.com>
2025-08-28 22:10:49 +0530

    Resolved conflict in index.html

---
+++ first.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const heading=React.createElement("h1",{id:"heading"},"jenish");
+
+    const root=ReactDom.createRoot(document.getElementById("root"));
+
+root.render(heading);
